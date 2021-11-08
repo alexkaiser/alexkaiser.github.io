06b52f6548607acf1cad9a4972d44a38d302d625
alexkaiser <adkaiser@gmail.com>
2021-11-07 23:09:09 -0800

    updated website 11 21

--- index/Alexander_D._Kaiser_files/Alexander_D._Kaiser.js
+++ index/Alexander_D._Kaiser_files/Alexander_D._Kaiser.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20201024
+// Created by iWeb 3.0.4 local-build-20211107
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
--- index/Software_files/Software.js
+++ index/Software_files/Software.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20201024
+// Created by iWeb 3.0.4 local-build-20211107
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
